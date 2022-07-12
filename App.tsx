bd7e45b5dcc9f9cc2454b7778f3a2c4ab5187587
Mustafa Can Genç <samfatuu@gmail.com>
2022-07-12 17:57:38 +0300

    theming

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import StackNav from './src/screens/nav/StackNav';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  version: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#63d471',
+    secondary: '#4D9DE0',
+    tertiary: '#a1b2c3'
+  },
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StackNav />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+            <StackNav />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 };
 
