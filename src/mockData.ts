bdd25a78c5449d6e7b6ce226494d3b5f255d9e0e
Mustafa Can Genç <samfatuu@gmail.com>
2022-07-14 00:37:30 +0300

    mock device list to test

---
+++ src/mockData.ts
@@ -0,0 +1,229 @@
+import { Device } from "./types";
+
+export const deviceList : Device[] = [
+  {
+    id: '123',
+    name: 'Television',
+    iconName: 'television',
+    watt: 50,
+    count: 1,
+    dailyHours: 10,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '234',
+    name: 'Fridge',
+    iconName: 'fridge',
+    watt: 44,
+    count: 1,
+    dailyHours: 24,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '112',
+    name: 'Cell Phone',
+    iconName: 'phone',
+    watt: 4,
+    count: 5,
+    dailyHours: 1,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '1233',
+    name: 'Television',
+    iconName: 'television',
+    watt: 50,
+    count: 1,
+    dailyHours: 10,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '2434',
+    name: 'Fridge',
+    iconName: 'fridge',
+    watt: 44,
+    count: 1,
+    dailyHours: 24,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '1125',
+    name: 'Cell Phone',
+    iconName: 'cell-phone',
+    watt: 4,
+    count: 5,
+    dailyHours: 1,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '1236',
+    name: 'Television',
+    iconName: 'television',
+    watt: 50,
+    count: 1,
+    dailyHours: 10,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '2374',
+    name: 'Fridge',
+    iconName: 'fridge',
+    watt: 44,
+    count: 1,
+    dailyHours: 24,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '1182',
+    name: 'Cell Phone',
+    iconName: 'cell-phone',
+    watt: 4,
+    count: 5,
+    dailyHours: 1,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '1293',
+    name: 'Television',
+    iconName: 'television',
+    watt: 50,
+    count: 1,
+    dailyHours: 10,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '23334',
+    name: 'Fridge',
+    iconName: 'fridge',
+    watt: 44,
+    count: 1,
+    dailyHours: 24,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '11442',
+    name: 'Cell Phone',
+    iconName: 'cell-phone',
+    watt: 4,
+    count: 5,
+    dailyHours: 1,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '121233',
+    name: 'Television',
+    iconName: 'television',
+    watt: 50,
+    count: 1,
+    dailyHours: 10,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '234534',
+    name: 'Fridge',
+    iconName: 'fridge',
+    watt: 44,
+    count: 1,
+    dailyHours: 24,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  },
+  {
+    id: '1112312',
+    name: 'Cell Phone',
+    iconName: 'cell-phone',
+    watt: 4,
+    count: 5,
+    dailyHours: 1,
+    weeklyDays: 7,
+    amounts: {
+      day: 1,
+      week: 2,
+      month: 3,
+      year: 4
+    }
+  }
+];
