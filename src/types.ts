80d1f0af2c666e2efb7d7f2360983b9815782e97
Mustafa Can Genç <samfatuu@gmail.com>
2022-07-14 00:37:12 +0300

    device type created

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,25 @@ export type StackParamList = {
   Settings: undefined;
   Information: undefined;
 }
+
+export class Device {
+  constructor(
+    public id: string,
+    public name: string,
+    public iconName: string,
+    public watt: number,
+    public count: number,
+    public dailyHours: number,
+    public weeklyDays: number,
+    public amounts: TotalAmount
+  ) {}
+}
+
+export class TotalAmount {
+  constructor(
+    public day: number,
+    public week: number,
+    public month: number,
+    public year: number
+  ) {}
+}
