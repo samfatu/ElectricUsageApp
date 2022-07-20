d88081b325726e6dcf79b113957df9c9a600304a
Mustafa Can Genç <samfatuu@gmail.com>
2022-07-20 20:35:34 +0300

    korean locales

--- src/i18n/ko.ts
+++ src/i18n/ko.ts
@@ -1,5 +1,35 @@
-const kr = {
-
+const ko = {
+  "app-title": "전기 가격 계산",
+  "welcome": "어서 오십시오!",
+  "description": "이 응용 프로그램에서는 모든 장치 또는 장치의 일일, 월별 및 연간 전기 소비를 계산하고 단일 페이지의 총 금액을 볼 수 있습니다.",
+  "calculate-menu": "전기 소비를 계산합니다",
+  "preferences-menu": "지역 설정을 변경하십시오",
+  "information-menu": "전기 소비에 대한 정보",
+  "no-device": "표시 할 장치가 없습니다.",
+  "monthly-total":	"월간 총 :",
+  "annual-total":	"연간 총 :",
+  "daily-error": "하루에 시간은 0과 24 사이 여야합니다.",
+  "weekly-error": "주당 값은 0과 7 사이 여야합니다.",
+  "choose-icon": "아이콘을 선택하십시오",
+  "add-device":	"기기 추가",
+  "device-name":	"장치 이름",
+  "watt":	"와트",
+  "piece-u":	"조각",
+  "piece-l":	"조각",
+  "day": "낮",
+  "week": "주",
+  "month": "월",
+  "year": "년도",
+  "hours-day":	"하루에 시간",
+  "days-week":	"주당 일",
+  "edit-device":	"장치 편집",
+  "save-device":	"장치 저장",
+  "delete-device":	"장치 삭제",
+  "choose-language":	"언어 선호도를 선택하십시오",
+  "choose-currency":	"통화 환경 설정을 선택하십시오",
+  "enter-price":	"당신의 나라에서 킬로와트 당 가격을 입력하십시오",
+  "save-preferences":	"환경 설정을 저장하십시오",
+  "preferences-success": "선호도가 성공적으로 변경되었습니다.",
 };
 
-export default kr;
+export default ko;
